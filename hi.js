192aa4a7075f47202329beba4943f1be4b32118d
Steven <cchhunsteven@gmail.com>
2023-09-05 22:28:05 -0400

    javascript is located in hi.js now

--- hi.js
+++ hi.js
@@ -1,5 +1,253 @@
-function sum(x, y) {
-    return x + y
+let characters = [
+    [
+        "Artoria Pendragon",
+        ["Artoria Pendragon", "Altria Pendragon", "Artoria", "Altria", "King Of Knights"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/b/be/S002_Stage4.webp"
+    ],
+    [
+        "Artoria Pendragon (Alter)",
+        ["Artoria Pendragon (Alter)", "Salter", "Artoria Alter", "Altria Alter", "Saber Alter", "Altria Pendragon (Alter)", "Black Saber"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/9/93/S003_Stage1.webp"
+    ],
+    [
+        "Gawain",
+        ["Gawain", "Sir Gawain", "Knight of the Sun", "White Knight of the Round Table"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/8/85/S123_Stage3.webp"
+    ],
+    [
+        "EMIYA",
+        ["EMIYA", "Emiya Shirou", "Shirou", "Shirou Emiya", "Nameless", "Faker", "Hero Of Justice", "Red Archer"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/6/60/S011_Stage4.webp"
+    ],
+    [
+        "Gilgamesh",
+        ["Gilgamesh", "Gil", "King Of Heroes"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/6/6a/S012_Stage4.webp"
+    ],
+    [
+        "Ishtar",
+        ["Ishtar", "Inanna", "Rin", "Rin Tohsaka", "Mistress of Heaven"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/9/92/S142_Stage2.webp"
+    ],
+    [
+        "Arash",
+        ["Arash", "Arash Kamangir"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/8/80/S016_Stage1.webp"
+    ],
+    [
+        "Heracles",
+        ["Heracles", "Herc", "Hercules"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/f/f4/S047_Stage1.webp"
+    ],
+    [
+        "Lancelot",
+        ["Lancelot", "Zerkerlot", "Black Knight", "Mad Dog"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/0/0b/S048_Stage1.webp"
+    ],
+    [
+        "Cú Chulainn",
+        ["Cú Chulainn", "Cu Chulainn", "Cú", "Cu", "Child Of Light", "Child of the Sun", "Hound of Ulster"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/7/75/S017_Stage1.webp"
+    ],
+    [
+        "Diarmuid Ua Duibhne",
+        ["Diarmuid Ua Duibhne", "Diarmuid"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/9/9a/S071_Stage1.webp"
+    ],
+    [
+        "Jaguar Man",
+        ["Jaguar Man", "Taiga", "Fujimura Taiga"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/8/88/S148_Stage1.webp"
+    ],
+    [
+        "Medusa",
+        ["Medusa", "Gorgon"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/e/e7/S023_Stage2.webp"
+    ],
+    [
+        "Ushiwakamaru",
+        ["Ushiwakamaru", "Ushi", "Minamoto no Yoshitsune"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/1/12/S027_Stage3.webp"
+    ],
+    [
+        "Astolfo",
+        ["Astolfo", "Rider of Black", "Trap"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/3/34/S094_Stage2.webp"
+    ],
+    [
+        "Iskander",
+        ["Iskander", "Alexander the Great", "King of Conquerors"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/f/f2/S108_Stage2.webp"
+    ],
+    [
+        "Medea",
+        ["Medea", "Witch of Betrayal"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/2/22/S031_Stage1.webp"
+    ],
+    [
+        "Hans Christian Andersen",
+        ["Hans Christian Andersen"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/8/84/S033_Stage2.webp"
+    ],
+    [
+        "Zhuge Liang (Lord El-Melloi II)",
+        ["Zhuge Liang", "Zhuge Liang (Lord El-Melloi II)", "Waver", "Waver Velvet", "Lord El-Melloi II", "Lord El-Melloi 2"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/f/f0/S037_Stage1.webp"
+    ],
+    [
+        "Merlin",
+        ["Merlin", "Magus of Flowers"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/4/42/S150_Stage3.webp"
+    ],
+    [
+        "Sasaki Kojirou",
+        ["Sasaki Kojirou", "Sasaki Kojiro", "Sasaki Kojirō", "Nameless Samurai", "Sasaki Ganryu"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/1/18/S039_Stage1.webp"
+    ],
+    [
+        "Cursed Arm Hassan",
+        ["Cursed Arm Hassan", "Hassan of the Cursed Arm", "True Assassin", "Hanam", "Assassin of the White Skull"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/b/bf/S040_Stage1.webp"
+    ],
+    [
+        "Ryougi Shiki",
+        ["Ryougi Shiki", "Ryōgi Shiki", "Shiki"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/8/80/S092_Stage1.webp"
+    ],
+    [
+        "EMIYA (Assassin)",
+        ["EMIYA (Asssasin)", "Emiya Kiritsugu", "Kiritsugu", "Magus Killer"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/b/b1/S109_Stage3.webp"
+    ],
+    [
+        "Mash Kyrielight",
+        ["Mashu", "Mash", "Mash Kyrielight", "Mashu Kirielite", "Galahad", "Sir Galahad", "Fairy Knight Galahad"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/b/b5/S001_Stage4.webp"
+    ],
+    [
+        "Frankenstein",
+        ["Frankenstein", "Fran", "Berserker of Black", "Innocent Bride"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/5/5d/S082_Stage1.webp"
+    ],
+    [
+        "Florence Nightingale",
+        ["Florence Nightingale", "Nightingale", "Florence", "Mercedes", "Angel of Crimea"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/4/4c/S097_Stage1.webp"
+    ],
+    [
+        "Jeanne d'Arc",
+        ["Jeanne d'Arc", "Joan of Arc", "Joan", "Jeanne", "St Joan", "Maiden of Orleans"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/b/b2/S059_Stage2.webp"
+    ],
+    [
+        "Amakusa Shirou",
+        ["Amakusa Shirō", "Amakusa Shirou", "Shirou Kotomine", "Shirō Kotomine"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/2/2a/S093_Stage2.webp",
+    ],
+    [
+        "Edmond Dantes",
+        ["Edmond Dantes", "Edmond Dantès", "The Count of Monte Cristo"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/f/fb/S096_Stage3.webp"
+    ],
+    [
+        "Jeanne d'Arc (Alter)",
+        ["Jeanne d'Arc (Alter)", "Jalter", "Dragon Witch", "Witch of Vengeance", "Jeanne Alter", "Joan Alter"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/1/11/S106_Stage2.webp"
+    ],
+    [
+        "Angra Mainyu",
+        ["Angra Mainyu", "Angra", "King of Daemons"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/7/7f/S107_Stage2.webp"
+    ],
+    [
+        "Mysterious Heroine XX",
+        ["Mysterious Heroine XX", "MHXX", "Mysterious Heroine X (Foreigner)", "Space Cop", "Space Saber"],
+        "https://static.wikia.nocookie.net/fategrandorder/images/9/92/S222_Stage4.webp",
+    ],
+];
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const servantImage = document.querySelector(".servantImage")
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let index;
+let characterInfo;
+let characterName;
+
+let guessCount;
+let resetButton;
+
+function validateName(inputName) {
+    for (const characterName of characterInfo[1]) {
+        if (characterName.toLowerCase() === inputName.toLowerCase()) {
+            return true;
+        }
+    }
 }
 
-console.log(sum(1, 5))
+function newGame() {
+    index = Math.floor(Math.random() * characters.length);
+    characterInfo = characters[index];
+    characterName = characterInfo[0];
+    servantImage.src = characterInfo[2];
+    //servantImage.alt = characterName
+
+    guessCount = 1;
+}
+
+function restart() {
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    resetButton.parentNode.removeChild(resetButton);
+    // lastResult.style.backgroundColor = "white";
+    
+    newGame();
+}
+
+function endGame() { // avengers
+    guessSubmit.disabled = true;
+    guessField.disabled = true;
+
+    lastResult.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Restart, bozo";
+    guessSubmit.after(resetButton) //document.body.after(guessSubmit, resetButton);
+    resetButton.addEventListener("click", restart);
+}
+
+function checkGuess() {
+    const userGuess = guessField.value;
+
+    if (userGuess === "") {
+        return;
+    }
+
+    if (validateName(userGuess.trim())) {
+        lastResult.textContent = `You guessed right, it was ${characterName}!`;
+        endGame();
+
+        return;
+    }
+    else if (guessCount > 2) {
+        lastResult.textContent = `You have massive skill issues, the answer was ${characterName}!`;
+        endGame();
+
+        return;
+    }
+
+    lastResult.textContent = `Guess : ${userGuess} (${3 - guessCount} left)`;
+
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+newGame();
